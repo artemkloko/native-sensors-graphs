980af375b67549156589e3194804864ce9ab6f9b
artemkloko <artemkloko@gmail.com>
2020-10-01 16:18:17 +0300

    feat: a basic implementation of sensorEvent emitting

--- src/App.tsx
+++ src/App.tsx
@@ -4,72 +4,41 @@ import {
   StyleSheet,
   ScrollView,
   View,
-  Text,
   StatusBar,
+  Button,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
 
-declare const global: {HermesInternal: null | {}};
-
-import {useSensorsModule} from 'sensors-module';
+import {useSensors} from 'sensors-module';
+import {useSocket} from './services/socket';
 
 export const App = () => {
-  const {register, unregister} = useSensorsModule((event) => {
-    // console.log(event);
+  const socket = useSocket();
+  const {register, unregister} = useSensors((event) => {
+    socket.send(event);
   });
 
   useEffect(() => {
-    // register();
-    // return unregister;
+    
+    return unregister;
   }, []);
 
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
+      <SafeAreaView style={{height: '100%'}}>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+          contentContainerStyle={styles.scrollView}>
           <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
           <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
+            <View style={styles.buttonContainer}>
+              <Button title={'START MONITORING'} onPress={register} />
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
+            <View style={styles.buttonContainer}>
+              <Button title="STOP MONITORING" onPress={unregister} />
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -80,38 +49,18 @@ export const App = () => {
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
   },
   body: {
     backgroundColor: Colors.white,
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  buttonContainer: {
+    marginTop: 40,
   },
 });
